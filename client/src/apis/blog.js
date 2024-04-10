198accebf6a2a9e678bf60e37f6ad4ad7679b107
Yash Gupta <yashgupta@microsoft.com>
2024-04-10 15:22:02 +0530

    Final Push

--- client/src/apis/blog.js
+++ client/src/apis/blog.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3000/api/"
+    baseURL: "https://nginx-proxy.icysky-8546baf3.westus2.azurecontainerapps.io/api/"
+    // baseURL: "http://localhost:3000/api/"
 });
